59cd31f64332c236c51e64719d551d2f1a659036
Daniel Chang <56414208+chanhodchang@users.noreply.github.com>
2020-05-22 01:37:46 -0700

    Added all the data values into the database tables in postgres.

--- sql_queries.py
+++ sql_queries.py
@@ -8,26 +8,13 @@ time_table_drop = "DROP TABLE IF EXISTS time_table"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS songplay_table
-(songplay_id int PRIMARY KEY,
-start_time date REFERENCES time_table(start_time),
-user_id int REFERENCES user_table(user_id),
-level varchar,
-song_id int REFERENCES song_table(song_id),
-artist_id int REFERENCES artist_table(artist_id),
-session_id int,
-location varchar,
-user_agent varchar)
-""")
-
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS user_table
 (user_id int PRIMARY KEY,
-first_name varchar NOT NULL,
-last_name varchar NOT NULL,
-gender varchar NOT NULL,
-level varchar NOT NULL)
+first_name varchar,
+last_name varchar,
+gender varchar,
+level varchar)
 """)
 
 song_table_create = ("""
@@ -42,35 +29,42 @@ duration float NOT NULL)
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artist_table(
     artist_id text PRIMARY KEY,
-    artist_name text NOT NULL,
-    artist_location text,
-    artist_latitude float,
-    artist_longitude float
+    name text NOT NULL,
+    location text,
+    latitude float,
+    longitude float
 )
 """)
 
 time_table_create = ("""
-CREATE TABLE IF NOT EXISTS time_table
-(start_time date PRIMARY KEY,
-hour int NOT NULL,
-day int NOT NULL,
-week int NOT NULL,
-month int NOT NULL,
-year int NOT NULL,
-weekday varchar NOT NULL)
+CREATE TABLE IF NOT EXISTS time_table(
+    start_time date PRIMARY KEY,
+    hour int,
+    day int,
+    week int,
+    month int,
+    year int,
+    weekday varchar
+)
+""")
+
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplay_table
+(songplay_id int PRIMARY KEY,
+start_time date REFERENCES time_table(start_time),
+user_id int REFERENCES user_table(user_id),
+level varchar,
+song_id text REFERENCES song_table(song_id),
+artist_id text REFERENCES artist_table(artist_id),
+session_id int,
+location varchar,
+user_agent varchar)
 """)
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
--- INSERT INTO songplay_table
-(songplay_id,start_time,user_id,level,song_id,artist_id,session_id,location,user_agent)
-VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
-ON CONFLICT (songplay_id) DO NOTHING;
-""")
-
 user_table_insert = ("""
--- INSERT INTO user_table
+INSERT INTO user_table
 (user_id,first_name,last_name,gender,level)
 VALUES(%s,%s,%s,%s,%s)
 ON CONFLICT (user_id) DO NOTHING;
@@ -84,26 +78,38 @@ ON CONFLICT (song_id) DO NOTHING;
 """)
 
 artist_table_insert = ("""
--- INSERT INTO artist_table
-(artist_id, artist_name, artist_location, artist_latitude,artist_longitude)
+INSERT INTO artist_table
+(artist_id,name,location,latitude,longitude)
 VALUES(%s,%s,%s,%s,%s)
 ON CONFLICT (artist_id) DO NOTHING;
 """)
 
 
 time_table_insert = ("""
--- INSERT INTO time_table
+INSERT INTO time_table
 (start_time,hour,day,week,month,year,weekday)
 VALUES(%s,%s,%s,%s,%s,%s,%s)
 ON CONFLICT (start_time) DO NOTHING;
 """)
 
+songplay_table_insert = ("""
+INSERT INTO songplay_table
+(songplay_id,start_time,user_id,level,song_id,artist_id,session_id,location,user_agent)
+VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
+ON CONFLICT (songplay_id) DO NOTHING;
+""")
+
 # FIND SONGS
 
 song_select = ("""
+SELECT song_id, artist_table.artist_id
+FROM song_table JOIN artist_table ON song_table.artist_id = artist_table.artist_id
+WHERE song_table.title = %s
+AND artist_table.name = %s
+AND song_table.duration = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [user_table_drop, song_table_drop, artist_table_drop, time_table_drop, songplay_table_drop]
